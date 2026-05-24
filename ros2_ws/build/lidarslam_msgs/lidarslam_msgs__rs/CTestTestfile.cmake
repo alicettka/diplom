4475f58a68f262da1a68916dbf8b4c9cc3c186bd
alicettka <134172462+alicettka@users.noreply.github.com>
2026-05-25 00:40:47 +0300

    Add files via upload

---
+++ ros2_ws/build/lidarslam_msgs/lidarslam_msgs__rs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alice/ros2_ws/build/lidarslam_msgs/lidarslam_msgs__rs
+# Build directory: /home/alice/ros2_ws/build/lidarslam_msgs/lidarslam_msgs__rs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
