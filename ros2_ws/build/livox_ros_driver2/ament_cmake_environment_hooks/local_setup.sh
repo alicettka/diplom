acb80b17efe00a6f01aa5e8dfcc847fab4ba59f5
alicettka <134172462+alicettka@users.noreply.github.com>
2026-05-25 01:17:07 +0300

    Add files via upload

--- ros2_ws/build/livox_ros_driver2/ament_cmake_environment_hooks/local_setup.sh
+++ ros2_ws/build/livox_ros_driver2/ament_cmake_environment_hooks/local_setup.sh
@@ -2,7 +2,7 @@
 
 # since this file is sourced use either the provided AMENT_CURRENT_PREFIX
 # or fall back to the destination set at configure time
-: ${AMENT_CURRENT_PREFIX:="/home/alice/ros2_ws/install/livox_ros_driver2"}
+: ${AMENT_CURRENT_PREFIX:="/home/alice/ros2_ws/install/scanmatcher"}
 if [ ! -d "$AMENT_CURRENT_PREFIX" ]; then
   if [ -z "$COLCON_CURRENT_PREFIX" ]; then
     echo "The compile time prefix path '$AMENT_CURRENT_PREFIX' doesn't " \
@@ -151,10 +151,9 @@ if [ -z "$AMENT_RETURN_ENVIRONMENT_HOOKS" ]; then
 fi
 
 # list all environment hooks of this package
-ament_append_value AMENT_ENVIRONMENT_HOOKS "$AMENT_CURRENT_PREFIX/share/livox_ros_driver2/environment/ament_prefix_path.sh"
-ament_append_value AMENT_ENVIRONMENT_HOOKS "$AMENT_CURRENT_PREFIX/share/livox_ros_driver2/environment/library_path.sh"
-ament_append_value AMENT_ENVIRONMENT_HOOKS "$AMENT_CURRENT_PREFIX/share/livox_ros_driver2/environment/path.sh"
-ament_append_value AMENT_ENVIRONMENT_HOOKS "$AMENT_CURRENT_PREFIX/share/livox_ros_driver2/environment/pythonpath.sh"
+ament_append_value AMENT_ENVIRONMENT_HOOKS "$AMENT_CURRENT_PREFIX/share/scanmatcher/environment/ament_prefix_path.sh"
+ament_append_value AMENT_ENVIRONMENT_HOOKS "$AMENT_CURRENT_PREFIX/share/scanmatcher/environment/library_path.sh"
+ament_append_value AMENT_ENVIRONMENT_HOOKS "$AMENT_CURRENT_PREFIX/share/scanmatcher/environment/path.sh"
 
 # source all shell-specific environment hooks of this package
 # if not returning them
