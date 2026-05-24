ac601e054b2b36a6a3ba4e5fe343c0c19ec3571b
alicettka <134172462+alicettka@users.noreply.github.com>
2026-05-25 00:09:37 +0300

    Add files via upload

---
+++ ros2_ws/build/lidarslam/colcon_command_prefix_build.sh
@@ -0,0 +1,5 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/alice/ros2_ws/install/lidarslam_msgs/share/lidarslam_msgs/package.sh"
+. "/home/alice/ros2_ws/install/ndt_omp_ros2/share/ndt_omp_ros2/package.sh"
+. "/home/alice/ros2_ws/install/graph_based_slam/share/graph_based_slam/package.sh"
+. "/home/alice/ros2_ws/install/scanmatcher/share/scanmatcher/package.sh"
